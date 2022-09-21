eb81c538fddb6d0616801e0c0fde1677864af5c9
Danshicu <Promchuk.danik@gmail.com>
2022-09-21 16:44:21 +0300

    added moving

--- Laba1_ppois/Laba1_ppois/Laba1_ppois.cpp
+++ Laba1_ppois/Laba1_ppois/Laba1_ppois.cpp
@@ -1,17 +1,10 @@
-﻿// Laba1_ppois.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-class point {
+class Point {
 private:
     int x, y;
 public:
-    /*point(int coord_x, int coord_y) {
-        x = coord_x;
-        y = coord_y;
-    }*/
    void  Show_coordinates() {
        cout << '(' << x << ", " << y << ')' << endl;
     }
@@ -25,27 +18,44 @@ public:
        x = coord_x;
        y = coord_y;
    }
+   void Up(int number) {
+       y += number;
+   }
+   void Right(int number) {
+       x += number;
+   }
+   void Down(int number) {
+       y -= number;
+   }
+   void Left(int number) {
+       x -= number;
+   }
 };
 
 class rectangle {
 private:
-    point point1, point2, point3, point4;
+    Point point[4];
 public:
-   /* rectangle() {
-         point2.Set_coordinates(point3.GetX(), point1.GetY());
-         point4.Set_coordinates(point1.GetX(), point3.GetY());
-    }*/
     void Show_rectangle_coordinates() {
-        point1.Show_coordinates();
-        point2.Show_coordinates();
-        point3.Show_coordinates();
-        point4.Show_coordinates();
+        for (auto& _point : point) {
+            _point.Show_coordinates();
+            cout << endl;
+        }
     }
     void Set_rectangle_coordinates(int X1, int X2, int Y1, int Y2) {
-        point1.Set_coordinates(X1, Y1);
-        point3.Set_coordinates(X2, Y2);
-        point2.Set_coordinates(point3.GetX(), point1.GetY());
-        point4.Set_coordinates(point1.GetX(), point3.GetY());
+        point[0].Set_coordinates(X1, Y1);
+        point[2].Set_coordinates(X2, Y2);
+        point[1].Set_coordinates(point[0].GetX(), point[2].GetY());
+        point[3].Set_coordinates(point[2].GetX(), point[0].GetY());
+    }
+    void MoveRectangle(int direction, int number) {
+        switch (direction) {
+        case 1: {for (auto& _point : point) { _point.Up(number); }break; }
+        case 2: {for (auto& _point : point) { _point.Right(number); }break; }
+        case 3: {for (auto& _point : point) { _point.Down(number); }break; }
+        case 4: {for (auto& _point : point) { _point.Left(number); }break; }
+        }
+
     }
 
 };
@@ -57,21 +67,10 @@ int main()
         cin >> y1;
         cin >> x2;
         cin >> y2;
-        //point A/*(x1, y1)*/;
-        //point B/*(x2, y2)*/;
-        rectangle ABCD/*(A, B)*/;
+        rectangle ABCD;
         ABCD.Set_rectangle_coordinates(x1, x2, y1, y2);
         ABCD.Show_rectangle_coordinates();
-    std::cout << "Hello World!\n";
+        ABCD.MoveRectangle(3, 10);
+        ABCD.Show_rectangle_coordinates();
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
